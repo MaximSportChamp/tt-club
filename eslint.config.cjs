19ff869e82064f4cf8ad14d9dba80ec910a9af9a
MaximSportChamp <maksim0301@gmail.com>
2025-08-16 22:42:05 +0300

    chore: configure eslint flat config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,8 +1,13 @@
+const js = require('@eslint/js');
+const vue = require('eslint-plugin-vue');
+const prettier = require('eslint-config-prettier');
+
 module.exports = [
+  js.configs.recommended,
+  ...vue.configs['flat/recommended'],
   {
+    files: ['src/**/*.{js,vue}'],
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
       globals: {
         defineProps: 'readonly',
         defineEmits: 'readonly',
@@ -10,6 +15,19 @@ module.exports = [
         withDefaults: 'readonly',
       },
     },
-    ignores: ['**/*.ts', '**/*.vue'],
+  },
+  {
+    files: ['tests/**/*.js'],
+    languageOptions: {
+      globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        expect: 'readonly',
+      },
+    },
+  },
+  prettier,
+  {
+    ignores: ['**/*.ts'],
   },
 ];
